9f1be997bcc11516867d6fee801b6e2947b06faf
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2026-03-09 09:58:45 -0300

    Commit Cp01 Cadastro

---
+++ Cadastro/Cadastro/Cadastro.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    public class Cadastro<T>
+    {
+        private Dictionary<int, T> dados = new Dictionary<int, T>();
+        private int proximoId = 1;
+
+        public void Adicionar(T item)
+        {
+            dados.Add(proximoId, item);
+            Console.WriteLine($"Item adicionado com sucesso! (ID gerado: {proximoId})");
+            proximoId++;
+        }
+
+        public void Listar()
+        {
+            if (dados.Count == 0)
+            {
+                Console.WriteLine("A lista está vazia.");
+                return;
+            }
+
+            foreach (var item in dados)
+            {
+                Console.WriteLine($"ID: {item.Key} | Dados: {item.Value}");
+            }
+        }
+
+        public void Buscar(int id)
+        {
+            if (dados.TryGetValue(id, out T item))
+            {
+                Console.WriteLine($"Encontrado: {item}");
+            }
+            else
+            {
+                Console.WriteLine("Item não encontrado.");
+            }
+        }
+
+        public void Remover(int id)
+        {
+            if (dados.Remove(id))
+            {
+                Console.WriteLine("Item removido com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("ID não encontrado para remoção.");
+            }
+        }
+    }
+
+}
---
+++ Cadastro/Cadastro/Program.cs
@@ -0,0 +1,65 @@
+﻿using Cadastro;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cadastro<Pessoa> cadastroPessoas = new Cadastro<Pessoa>();
+            bool rodando = true;
+
+            while (rodando)
+            {
+                Console.WriteLine("\n--- MENU DE CADASTRO ---");
+                Console.WriteLine("1 - Adicionar");
+                Console.WriteLine("2 - Listar");
+                Console.WriteLine("3 - Buscar");
+                Console.WriteLine("4 - Remover");
+                Console.WriteLine("5 - Sair");
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Write("Digite o Nome Completo: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Digite a Idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+
+
+                        cadastroPessoas.Adicionar(new Pessoa { Nome = nome, Idade = idade });
+                        break;
+
+                    case "2":
+                        cadastroPessoas.Listar();
+                        break;
+
+                    case "3":
+                        Console.Write("Digite o ID para buscar: ");
+                        int idBusca = int.Parse(Console.ReadLine());
+                        cadastroPessoas.Buscar(idBusca);
+                        break;
+
+                    case "4":
+                        Console.Write("Digite o ID para remover: ");
+                        int idRemover = int.Parse(Console.ReadLine());
+                        cadastroPessoas.Remover(idRemover);
+                        break;
+
+                    case "5":
+                        rodando = false;
+                        Console.WriteLine("Saindo do sistema...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+        }
+    }
+}
